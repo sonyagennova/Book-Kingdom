fe4796ce6b5c41269bc3a33ebda6f798f776a6b3
Sonya Genova <sonyagennova@gmail.com>
2024-02-14 21:18:50 +0200

    update

--- NOIT2024/server/index.js
+++ NOIT2024/server/index.js
@@ -22,7 +22,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(express.static("public"));
+app.use(express.static("static"));
 //app.use(cors());
 
 app.use(express.static('public'))
