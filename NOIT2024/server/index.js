61f06e7446fafa01cf811cfb7b3aba5526c9cc4e
Sonya Genova <sonyagennova@gmail.com>
2024-02-14 20:36:42 +0200

    update

--- NOIT2024/server/index.js
+++ NOIT2024/server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-import createProxyMiddleware from "http-proxy-middleware";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 // import cors from './middlewares/cors.js';
 import cors from 'cors';
