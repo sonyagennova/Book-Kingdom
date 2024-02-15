b2e181d3d1f7fea31749be57d595dc89d0380e31
Sonya Genova <sonyagennova@gmail.com>
2024-02-15 11:40:44 +0200

    no message

--- NOIT2024/server/index.js
+++ NOIT2024/server/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: "book-kingdom-client.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
