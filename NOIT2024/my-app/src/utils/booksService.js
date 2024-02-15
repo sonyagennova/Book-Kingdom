1dd8d319f2fcf1512ec0bb5d3d24c11c8520f50b
Sonya Genova <sonyagennova@gmail.com>
2024-02-16 00:22:52 +0200

    update

--- NOIT2024/my-app/src/utils/booksService.js
+++ NOIT2024/my-app/src/utils/booksService.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:5500/data/books';
+const baseUrl = 'https://the-books-kingdom-server.onrender.com/data/books';
 const token = localStorage.getItem("accessToken");
 
 export const getAll = async () => {
