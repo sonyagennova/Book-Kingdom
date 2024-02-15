bd0273563801b9e6b3ad43dd0e3feaaeefc773db
Sonya Genova <sonyagennova@gmail.com>
2024-02-15 23:32:39 +0200

    update

--- NOIT2024/server/index.js
+++ NOIT2024/server/index.js
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
     origin: "https://the-books-kingdom.onrender.com/",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     optionsSuccessStatus: 200
 }));
