0b2dfa46aaf59706e1b22f03d75cc88db75e3c49
Sonya Genova <sonyagennova@gmail.com>
2024-02-26 23:37:29 +0200

    update

--- NOIT2024/my-app/src/components/otherPages/createPage.jsx
+++ NOIT2024/my-app/src/components/otherPages/createPage.jsx
@@ -73,7 +73,7 @@ export function CreatePage(){
               <input type="text" name="publication_year" placeholder="Година на издаване" />
             </div>
             <div>
-              <input type="text" name="description" placeholder="Резюме" rows="5" cols="80"/>
+              <textarea type="text" name="description" placeholder="Резюме" rows="5" cols="80"/>
             </div>
             <div>
               <input type="text" name="imageUrl" placeholder="URL линк за корица" />
