53d6b8a954ce2d96ba8ce289b6ddbd7793a5f337
Sonya Genova <sonyagennova@gmail.com>
2024-02-27 13:55:24 +0200

    update

--- NOIT2024/my-app/src/utils/logout.js
+++ NOIT2024/my-app/src/utils/logout.js
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import * as userService from "./userService"
 
 export async function logout(e){
     e.preventDefault()
 
+    const navigate = useNavigate()
+
     userService.logout();
+
+    navigate("/")
 
     //location.href = "/"
 }
